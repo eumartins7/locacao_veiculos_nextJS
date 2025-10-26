3d292cb0c40e2079e5dff6dcfe7dcfdf5372065a
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-10-25 23:14:40 -0300

    feat: seçao add (contato)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/components/header/header";
 import { Carousel } from "@/components/section-carousel/sectionCarousel";
 import { WhyService } from "@/components/infos-services/infosServices";
+import { CarsSection } from "@/components/section-cars/sectionCars";
+import { SectionContact } from "@/components/section-contact/sectionContact";
 
 
 export default function Home() {
@@ -9,6 +11,8 @@ export default function Home() {
       <Header />
       <Carousel />
       <WhyService  />
+      <CarsSection />
+      <SectionContact />
    </>
   );
 }
