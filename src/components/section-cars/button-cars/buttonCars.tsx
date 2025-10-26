67f1d051cfc7b29186140e5cdb4c9c6ac21e6e75
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-10-26 20:23:36 -0300

    style: estilo do botao melhorado

--- src/components/section-cars/button-cars/buttonCars.tsx
+++ src/components/section-cars/button-cars/buttonCars.tsx
@@ -2,7 +2,7 @@
 
 export function CarButton() {
     return (
-      <button className="bg-amber-600 p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer">
+      <button className="bg-amber-500 hover:bg-amber-600 p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer">
         Ver mais
       </button>  
     )
