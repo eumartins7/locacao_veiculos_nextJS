f34875fba811ae688fe86ee0de185440c4297b9d
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-10-26 20:21:44 -0300

    fix: img logo e fonte

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Header() {
     return (
-        <header>
+        <header id="inicio">
             <div className="flex bg-linear-to-r from-amber-500 to-white justify-between p-5 pl-20 pr-20">
                 <div className="flex items-center text-center">
-                    <h1 className="font-sans text-3xl font-bold">Martins Locações</h1>
+                    <h1 className="font-vehicle text-3xl font-bold">Martins Locações</h1>
                     <Image
-                    src='/logo/logo2.png'
+                    src='/logo/logoICON.png'
                     alt="Logo Martins Locações"
                     width={120}
                     height={120}
