c9fb3ff17a3d746147ed32b17307ed6acf43a7d7
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-10-30 23:23:00 -0300

    pasta onde ficam os componentes globais

---
+++ src/components/layout/botao-vendedor/button.tsx
@@ -0,0 +1,16 @@
+
+
+export function BotaoVendedor() {
+    return (
+        <div>
+            <a 
+                href="https://wa.me/5512988372717"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=" text-white border bg-amber-700 hover:bg-amber-500 p-1 pl-3 pr-3 m-4 rounded-lg cursor-pointer"
+            >
+                Fale com um vendedor
+            </a>
+        </div>
+    )
+}
---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+
+export function Header() {
+    return (
+        <header id="inicio">
+            <div className="flex bg-gradient-to-r from-amber-500 to-white justify-between p-5 pl-20 pr-20">
+                <div className="flex items-center text-center">
+                    <h1 className="font-vehicle text-3xl font-bold">Martins Locações</h1>
+                    <Image
+                    src='/logo/logoICON.png'
+                    alt="Logo Martins Locações"
+                    width={120}
+                    height={120}
+                    priority
+                    />  
+                </div>
+
+                <nav>
+                    <ul className="flex gap-3">
+                        <li><a href="#sobre">Sobre Nós</a></li>
+                        <li><a href="#veiculos">Veículos</a></li>
+                        <li><a href="#contato">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
