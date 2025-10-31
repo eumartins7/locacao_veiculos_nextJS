72ee2c1d9ca9fe4406b631be528dc8d559be577e
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-10-30 23:17:33 -0300

    fix: arrumado .map e adicionado rota dinamica

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,7 +1,7 @@
-import { ChatWidget } from "@/components/chat-widget/chatWidget"
-import { AllCars } from "@/components/page-cars/all-cars/allCars"
-import { FooterCars } from "@/components/page-cars/footer-cars/footerCars"
-import { HeaderCars } from "@/components/page-cars/header-cars/headerCar"
+import { ChatWidget } from "@/components/layout/chat-widget/chatWidget"
+import { AllCars } from "@/components/pagina-carros/todos-carros/allCars"
+import { FooterCars } from "@/components/pagina-carros/footer-cars/footerCars"
+import { HeaderCars } from "@/components/pagina-carros/header-cars/headerCar"
 
 
 export default function Cars() {
