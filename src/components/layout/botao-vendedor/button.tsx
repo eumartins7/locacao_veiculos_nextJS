efa96a653408df89719622fefad829a79e5df14f
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-01 22:01:20 -0300

    style: novo estilo ao botao de falar com o vendedor

--- src/components/layout/botao-vendedor/button.tsx
+++ src/components/layout/botao-vendedor/button.tsx
@@ -7,7 +7,7 @@ export function BotaoVendedor() {
                 href="https://wa.me/5512988372717"
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" text-white border bg-amber-700 hover:bg-amber-500 p-1 pl-3 pr-3 m-4 rounded-lg cursor-pointer"
+                className="text-white text-lg bg-amber-700 hover:bg-amber-600 p-2 pl-20 pr-20 rounded-xl cursor-pointer"
             >
                 Fale com um vendedor
             </a>
