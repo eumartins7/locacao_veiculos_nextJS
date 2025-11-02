b91fd140f25c7b56607e75212bb531fd29ea3327
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-01 21:57:12 -0300

    style: novo estilo ao botao que leva à página de veiculos

--- src/components/home/section-cars/button-page/buttonPage.tsx
+++ src/components/home/section-cars/button-page/buttonPage.tsx
@@ -1,12 +1,12 @@
-
+import Link from "next/link"
 
 export function ButtonPage() {
     return (
         <div className="w-[20%] border border-amber-600 hover:bg-amber-600 hover:text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer">
-            <a 
-            href="/cars">
-                <span>Ver todos os carros</span>
-            </a>
+        <Link href="/cars">
+            <span>Ver todos os carros</span>
+        </Link>
+            
         </div>
     )
 }
