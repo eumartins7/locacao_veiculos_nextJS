ad88aae375cd1532cef020bee509dea814297a96
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-04 20:56:13 -0300

    lembrete: apagar esta secao e ver o que por no lugar

--- src/components/home/section-contact/sectionContact.tsx
+++ src/components/home/section-contact/sectionContact.tsx
@@ -3,16 +3,17 @@ import { InputBox } from "./box-input/inputBox";
 
 export function SectionContact() {
     return(
-        <section id="contato" className="bg-gray-200 w-full mt-10 pt-20 pb-20 ">
-            <div className="flex justify-between items-center">
-                <div className="ml-20">
-                    <h1 className="font-contact text-6xl">
+        <section id="contato" className=" bg-gray-200 mt-10 pt-10 pb-10 ">
+            <div className="flex flex-col justify-center lg:justify-between">
+                <div className="text-center">
+                    <h1 className="font-contact text-2xl lg:text-6xl">
                         Alguma sugestão? <br />
                         Nos envie!
                     </h1>
                 </div>
 
                 <InputBox />
+        
             </div>
         </section>
     )
