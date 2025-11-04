a206d79c79930ea03b39110664656abec295be9a
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-04 20:51:21 -0300

    responsividade da secao 'sobre nos' adicionada

--- src/components/home/infos-services/infosServices.tsx
+++ src/components/home/infos-services/infosServices.tsx
@@ -3,32 +3,31 @@ import { CardAbout } from "./card-services/cardServices";
 
 export function WhyService() {
     return (
-        <section id="sobre" className="bg-gray-200 w-full mt-10 pt-20 pb-20">
-            <div className="flex justify-between items-center ml-20 mr-20">
-                <div className="font-service text-6xl">
-                    <h1>Por que escolher <br /> nossos serviços?
-                    </h1>
+        <section id="sobre" className="bg-gray-200 w-full mt-10 pt-10 pb-10">
+            <div className=" lg:flex justify-between items-center lg:ml-20 lg:mr-20">
+                <div className="flex justify-center gap-2 font-service text-3xl lg:text-6xl">
+                    <span>Porque escolher <br className="invisible lg:flex" /> nossos serviços?</span>
                 </div>
 
-                <div>
-                    <div className="flex flex-col gap-8 flex-1 max-w-lg">
+                <div className="flex justify-center pl-3 pr-3 mt-4">
+                    <div className="flex flex-col gap-2 min-w-[300px] w-[100%] lg:flex lg:flex-col lg:gap-8 lg:flex-1 lg:max-w-lg">
                         <CardAbout 
-                            title="Facilidade em Contratar"
+                            title="Facilidade em contratar"
                             description="Alugue seu carro sem burocracia, com atendimento rápido e direto."
                         />
 
                         <CardAbout 
-                            title="Para Quem Precisa de Mobilidade"
+                            title="Para quem precisa de mobilidade"
                             description="Atendemos motoristas particulares, profissionais autônomos, e qualquer pessoa que precise de um carro por um mês ou mais."
                         />
 
                         <CardAbout 
-                            title="Assistência 24 Horas"
+                            title="Assistência 24 horas"
                             description="Conte com suporte completo durante todo o período de locação, em qualquer imprevisto."
                         />
 
                         <CardAbout 
-                            title="Veículos com Manutenção em Dia"
+                            title="Veículos com manutenção em dia"
                             description="Nossa frota é revisada regularmente, garantindo segurança e tranquilidade em cada trajeto."
                         />
                     </div>
