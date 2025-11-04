a3f10f4512c8487c66279730b165d6b6be26c9ae
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-04 20:59:46 -0300

    mudanca no estilo e responsividade adicionada

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export function Header() {
     return (
         <header id="inicio">
-            <div className="flex bg-gradient-to-r from-amber-500 to-white justify-between p-5 pl-20 pr-20">
+            <div className="flex bg-gradient-to-r from-[#fc4a1a] to-[#f7b733] justify-between whitespace-nowrap pl-3 pr-3">
                 <div className="flex items-center text-center">
-                    <h1 className="font-vehicle text-3xl font-bold">Martins Locações</h1>
+                    <h1 className="font-vehicle text-xl md:text-3xl font-bold md:p-3">Martins Locações</h1>
                     <Image
                     src='/logo/logoICON.png'
                     alt="Logo Martins Locações"
@@ -16,8 +16,8 @@ export function Header() {
                     />  
                 </div>
 
-                <nav>
-                    <ul className="flex gap-3">
+                <nav className="invisible md:visible md:flex md:pr-3">
+                    <ul className="md:flex md:gap-3">
                         <li><a href="#sobre">Sobre Nós</a></li>
                         <li><a href="#veiculos">Veículos</a></li>
                         <li><a href="#contato">Contato</a></li>
