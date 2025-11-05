eaebaa2d00971f663f9e6297e866bd89047b16c7
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-04 21:33:59 -0300

    responsividade adicionada ao botao que acessa a pagina de carros

--- src/components/home/section-cars/button-page/buttonPage.tsx
+++ src/components/home/section-cars/button-page/buttonPage.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function ButtonPage() {
     return (
-        <div className="w-[20%] border border-amber-600 hover:bg-amber-600 hover:text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer">
+        <div className="w-[100%] border border-amber-600 hover:bg-amber-600 hover:text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer md:w-[50%]">
         <Link href="/cars">
             <span>Ver todos os carros</span>
         </Link>
