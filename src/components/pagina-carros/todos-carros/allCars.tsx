c1d73943d2d3cf1d1e0455a6770b4b9db85ad329
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-04 21:03:47 -0300

    mudanca no estilo e responsividade adicionada

--- src/components/pagina-carros/todos-carros/allCars.tsx
+++ src/components/pagina-carros/todos-carros/allCars.tsx
@@ -4,13 +4,13 @@ import { CardPageTwo } from "./pageCar-card/card"
 
 export function AllCars() {
     return (
-        <section id="topo" className="bg-gray-200 mt-10">
-            <div className=" flex flex-col  p-10">
-                <div className="font-vehicle text-center text-5xl">
+        <section id="topo" className="bg-gray-200 p-10">
+            <div className="flex flex-col gap-5 pr-5 pl-5 md:flex md:flex-col md: md:p-10">
+                <div className="flex justify-center whitespace-nowrap pt-10 text-2xl font-vehicle text-center md:text-3xl">
                     <h1>Confira nossos veículos!</h1>
                 </div>
 
-                <div className="flex justify-center flex-wrap gap-6 p-5">
+                <div className="md:flex md:justify-center md:flex-wrap md:gap-6">
                     {carDetails.map((car, index) =>(
                         <CardPageTwo 
                             key={index}
