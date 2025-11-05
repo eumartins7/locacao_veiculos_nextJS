9dee9eb3f4ed2b593fcd1922167ba061e60c7f92
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-05 19:15:23 -0300

    novo icone

--- src/components/home/infos-services/card-services/cardServices.tsx
+++ src/components/home/infos-services/card-services/cardServices.tsx
@@ -1,19 +1,33 @@
-
+import Image from "next/image"
 
 type CardAboutProps = {
     title: string
     description: string
+    image:string
 }
 
-export function CardAbout({title, description}: CardAboutProps){
+export function CardAbout({title, description, image}: CardAboutProps){
     return (
-        <div className=" bg-white text-center p-3 lg:p-6 rounded-2xl">
-            <h1 className="font-h1serv text-lg lg:text-3xl">
-                {title}
-            </h1>
-            <p className="font-pserv text-sm p-2 lg:p-5">
-                {description}
-            </p>
+        <div className="flex items-center bg-white text-left p-3 lg:p-6 rounded-2xl gap-4">
+            <div className="flex items-center">
+                <Image 
+                src={image}
+                alt=''
+                width={30}
+                height={30}
+                />
+            </div>
+            <div className="flex flex-col">
+               <h1 className="font-h1serv text-lg lg:text-3xl">
+                    {title}
+                </h1> 
+
+                <p className="font-pserv text-sm lg:text-base text-stone-600">
+                    {description}
+                </p>
+            </div>
+            
+            
         </div>
     )
 }
