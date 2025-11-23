b1002f5d13ab953278b2bfc1ce2fc4d6851130f4
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 20:57:01 -0300

    style: novo estilo ao botao

--- src/components/home/section-cars/button-page/buttonPage.tsx
+++ src/components/home/section-cars/button-page/buttonPage.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function ButtonPage() {
     return (
-        <div className="w-[100%] border border-amber-600 hover:bg-amber-600 hover:text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer md:w-[50%]">
+        <div className="w-[100%] bg-[#FF4100] hover:bg-[#FF6000] text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer md:w-[50%] font-aboutCompany font-bold">
         <Link href="/cars">
             <span>Ver todos os carros</span>
         </Link>
