6ffb9b2cdfe29f992f4febd2d528cd6c58bde981
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 20:49:39 -0300

    style: footer correto adicionado a page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import { Header } from "@/components/layout/header/header";
 import { Carousel } from "@/components/home/section-carousel/sectionCarousel";
 import { WhyService } from "@/components/home/infos-services/infosServices";
 import { CarsSection } from "@/components/home/section-cars/sectionCars";
-import { SectionContact } from "@/components/home/section-contact/sectionContact";
+import { SectionReview } from "@/components/home/section-reviews/sectionReviews";
 import { Footer } from "@/components/layout/footer/footer";
 import { ChatWidget } from "@/components/layout/chat-widget/chatWidget";
+import { SectionFaq } from "@/components/home/section-FAQ/sectionFAQ";
 
 
 export default function Home() {
@@ -15,7 +16,8 @@ export default function Home() {
       <ChatWidget />
       <WhyService  />
       <CarsSection />
-      <SectionContact />
+      <SectionReview />
+      <SectionFaq />
       <Footer />
    </>
   );
