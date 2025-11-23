7845a47fe0bbc70b473cd2e895ba69b9f6d25eb1
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 20:57:38 -0300

    style: novo estilo ao botao

--- src/components/home/section-cars/button-card/buttonCars.tsx
+++ src/components/home/section-cars/button-card/buttonCars.tsx
@@ -3,7 +3,7 @@
 export function CarButton() {
     return (
       <div 
-        className="font-aboutCompany border border-amber-600 hover:bg-amber-600 hover:text-white p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer"
+        className="font-aboutCompany bg-[#FF4100] hover:bg-[#FF6000] text-white font-bold p-1 pl-3 pr-3 m-4 rounded-xl cursor-pointer"
       >
         <span>Ver mais</span>
       </div>
