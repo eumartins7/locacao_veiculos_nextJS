da98e8fc52468ed1b6570446f45ad6b251d645c7
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 20:55:26 -0300

    refactor: substitui carrossel manual por swiper

--- src/components/home/section-carousel/sectionCarousel.tsx
+++ src/components/home/section-carousel/sectionCarousel.tsx
@@ -1,39 +1,49 @@
 'use client'
-import { useState } from "react"
-import { BotaoCarousel } from "@/components/home/section-carousel/button-carousel/buttonCarousel"
+import { Swiper, SwiperSlide } from "swiper/react"
 import Image from "next/image"
 
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 export function Carousel() {
-  const slides = ['/carousel/imgteste.png', '/carousel/imgteste2.png']
-  const [indexAtual, setIndexAtual] = useState(0)
-
-  const prevSlide = () => {
-    setIndexAtual((prev) => (prev === 0 ? slides.length - 1 : prev - 1))
-  }
-
-  const nextSlide = () => {
-    setIndexAtual((prev) => (prev === slides.length - 1 ? 0 : prev + 1))
-  }
+  const slides = [
+    {id:'1', image:'/carousel/banner1.png', alt:'banner1'},
+    {id:'2', image:'/carousel/banner2test.png', alt:'banner2'},
+  ]
 
   return (
     <section className="flex justify-center pt-10">
-      <div className="relative w-[80%] h-[150px] lg:w-7xl lg:h-[500px] overflow-hidden rounded-2xl">
-        {slides.map((slide, index) => (
-          <Image
-            key={index}
-            src={slide}
-            alt={`Slide ${index + 1}`}
+      <div className="relative w-[80%] h-[150px] lg:h-[600px] overflow-hidden rounded-2xl">
+        <Swiper
+        modules={[Autoplay, Navigation, Pagination]}
+        slidesPerView={1}
+        loop
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        className="!h-full"
+        >
+          {slides.map((item) => (
+          <SwiperSlide
+          key={item.id}
+          className="relative"
+          >
+            <Image 
+            src={item.image}
+            alt={item.alt}
             fill
-            className={`object-cover  ${
-              index === indexAtual ? "opacity-100 z-10" : "opacity-0 z-0"
-            }`}
+            className="object-cover"
             sizes="100vw"
-            priority={index === 0} // Apenas o primeiro slide recebe prioridade
-          />
+            />
+          </SwiperSlide>
         ))}
-
-        {/* o componente de botões precisa estar dentro de um pai 'relative' */}
-        <BotaoCarousel prev={prevSlide} next={nextSlide} />
+        </Swiper>
       </div>
     </section>
   )
