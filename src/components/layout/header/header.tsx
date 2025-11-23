9cd903150b950e0c010c758ae7dfdedb58946eb6
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 20:59:23 -0300

    style: novo estilo ao header 'global'

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -4,23 +4,25 @@ import Image from "next/image";
 export function Header() {
     return (
         <header id="inicio">
-            <div className="flex bg-gradient-to-r from-[#fc4a1a] to-[#f7b733] justify-between whitespace-nowrap pl-3 pr-3">
-                <div className="flex items-center text-center">
-                    <h1 className="font-vehicle text-xl md:text-3xl font-bold md:p-3">Martins Locações</h1>
+            <div className="flex bg-[#FF4100] justify-between whitespace-nowrap pr-3 md:px-20 ">
+                <div className="flex items-center text-center pl-3">
+                    <h1 className="font-vehicle text-xl md:text-3xl font-bold text-black">
+                        Martins Locações
+                    </h1>
                     <Image
-                    src='/logo/logoICON.png'
+                    src='/logo/logo.png'
                     alt="Logo Martins Locações"
-                    width={120}
-                    height={120}
+                    width={150}
+                    height={150}
                     priority
                     />  
                 </div>
 
-                <nav className="invisible md:visible md:flex md:pr-3">
-                    <ul className="md:flex md:gap-3">
-                        <li><a href="#sobre">Sobre Nós</a></li>
-                        <li><a href="#veiculos">Veículos</a></li>
-                        <li><a href="#contato">Contato</a></li>
+                <nav className="hidden md:visible md:flex md:text-center md:items-center">
+                    <ul className="md:flex md:text-stone-900 md:gap-3 md:font-aboutCompany md:font-bold">
+                        <li className="lg:hover:underline"><a href="#sobre">Sobre Nós</a></li>
+                        <li className="lg:hover:underline"><a href="#veiculos">Veículos</a></li>
+                        
                     </ul>
                 </nav>
             </div>
