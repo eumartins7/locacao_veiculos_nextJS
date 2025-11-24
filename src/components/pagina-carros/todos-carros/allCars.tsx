2af143c0f831aec99922b58fa6f2a83a1631dab2
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-11-23 21:02:49 -0300

    style: correcao no estilo do texto da pagina

--- src/components/pagina-carros/todos-carros/allCars.tsx
+++ src/components/pagina-carros/todos-carros/allCars.tsx
@@ -6,11 +6,11 @@ export function AllCars() {
     return (
         <section id="topo" className="bg-gray-200 p-10">
             <div className="flex flex-col gap-5 pr-5 pl-5 md:flex md:flex-col md: md:p-10">
-                <div className="flex justify-center whitespace-nowrap pt-10 text-2xl font-vehicle text-center md:text-3xl">
+                <div className="flex justify-center whitespace-nowrap pt-10 text-2xl font-vehicle text-center md:text-3xl lg:text-4xl">
                     <h1>Confira nossos veículos!</h1>
                 </div>
 
-                <div className="md:flex md:justify-center md:flex-wrap md:gap-6">
+                <div className="md:flex md:justify-center md:flex-wrap md:gap-6 lg:gap-10">
                     {carDetails.map((car, index) =>(
                         <CardPageTwo 
                             key={index}
