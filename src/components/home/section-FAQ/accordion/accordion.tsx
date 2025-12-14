aca6057246655533a41213b2dd1379ad2478a645
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-12-14 09:59:59 -0300

    add: adicionado accordion do shadcn UI

--- src/components/home/section-FAQ/accordion/accordion.tsx
+++ src/components/home/section-FAQ/accordion/accordion.tsx
@@ -1,41 +1,33 @@
 "use client"
-import { useState } from "react"
-import { ChevronDown } from "lucide-react";
+
+import { 
+    Accordion,
+    AccordionItem,
+    AccordionTrigger,
+    AccordionContent,
+} from "@/components/ui/accordion"
 
 
 type FaqProps = {
-    pergunta?:string,
-    resposta?:string
+    pergunta:string,
+    resposta:string
 }
 
-export function Accordion({pergunta, resposta}:FaqProps) {
+export function FaqAccordion({pergunta, resposta}:FaqProps) {
 
-    const [openItem, setOpenItem] = useState<string|null>(null)
-    const handleItemClick = (value:string) => {
-         setOpenItem(openItem===value?null:value)
-        }
 
     return (
-        <div className="rounded-md sm:p-9">
-            <div className="w-full">
-                <div className="rounded-md">
-                    <button
-                        onClick={() => handleItemClick("item-1")}
-                        className="flex justify-between w-full px-4 py-2 text-left text-lg font-bold cursor-pointer text-white hover:underline dark:text-black">
+        <div className="px-5 md:px-0 p-5">
+            <Accordion type="single" collapsible>
+                <AccordionItem value="item">
+                    <AccordionTrigger>
                         {pergunta}
-                        <ChevronDown />
-                    </button>
-                  
-
-                    <div
-                        style={{maxHeight:openItem==="item-1"?"none":0}}
-                        className="overflow-hidden">
-                            <div className=" font-aboutCompany font-medium p-4 ">
-                                <p>{resposta}</p>
-                            </div>
-                    </div>
-                </div> 
-            </div>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        {resposta}
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </div>
     )
 }
