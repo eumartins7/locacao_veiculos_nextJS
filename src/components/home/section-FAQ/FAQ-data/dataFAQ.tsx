61bcfba3bc0a0fd53419429b1665971d11c4ba30
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-12-14 09:57:53 -0300

    add: adicionado id's aos arrays do FAQ

--- src/components/home/section-FAQ/FAQ-data/dataFAQ.tsx
+++ src/components/home/section-FAQ/FAQ-data/dataFAQ.tsx
@@ -1,17 +1,21 @@
 export const FAQ = [
     {
+        id: "1",
         pergunta: 'Quais documentos são necessários para alugar um carro?',
         resposta:'Para alugar, você precisa apresentar CNH válida, documento com foto e comprovante de residência atualizado.'
     },
     {
+        id: "2",
         pergunta: 'Há taxa para cancelar a reserva?',
         resposta:'Cancelamentos feitos com até 24 horas de antecedência não possuem taxa. Após esse prazo, pode haver cobrança simbólica para bloqueio de agenda.'
     },
     {
+        id: "3",
         pergunta: 'Vocês trabalham com aplicativos de corrida (Uber, 99, etc.)?',
         resposta:'Sim. Temos planos específicos para motoristas de aplicativo, com veículos revisados e condições especiais.'
     },
     {
+        id: "4",
         pergunta: 'Como funciona a manutenção durante a locação?',
         resposta:'Toda manutenção preventiva e corretiva é por conta da locadora. Caso note algum problema, basta entrar em contato e faremos a substituição do veículo.'
     }
