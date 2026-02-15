9652381cd046694eaa52aad71425a276dc45ae62
Gabriel Martins <gabrielmartins8802@gmail.com>
2026-02-15 07:44:33 -0300

    fix: corrigio erro de caminho de componente

--- src/app/carros/page.tsx
+++ src/app/carros/page.tsx
@@ -1,12 +1,12 @@
 import { ChatWidget } from "@/feature/layout/chat-widget/chatWidget";
 import { AllCars } from "./components/pagina-carros/todos-carros/allCars";
 import { Footer } from "@/feature/layout/footer/footer";
-import { HeaderCars } from "./components/pagina-carros/header-cars/headerCar";
+import { Header } from "@/feature/layout/header/header";
 
 export default function Cars() {
   return (
     <>
-      <HeaderCars />
+      <Header />
       <AllCars />
       <Footer />
       <ChatWidget />
