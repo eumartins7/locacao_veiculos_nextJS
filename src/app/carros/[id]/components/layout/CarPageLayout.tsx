c2673bb05e3dafe5e4607bbf4cd04fc0a2aace7f
Gabriel Martins <gabrielmartins8802@gmail.com>
2026-03-28 14:18:06 -0300

    feat(ui): adicionado componente breadcrumbs

--- src/app/carros/[id]/components/layout/CarPageLayout.tsx
+++ src/app/carros/[id]/components/layout/CarPageLayout.tsx
@@ -1,5 +1,6 @@
 import { BoxDetalhes } from "./detalhes";
 import { Car } from "@/feature/cars/types/car";
+import { Breadcrumbs } from "@/feature/layout/bread-crumbs/Breadcrumbs";
 
 interface CarPageProps {
   carro: Car;
@@ -10,6 +11,9 @@ export function CarPage({ carro }: CarPageProps) {
     <main className="bg-gray-100 pb-10 pt-20">
       {/* Centralizador principal */}
       <div className="max-w-7xl mx-auto px-5">
+        <div className="m-8">
+          <Breadcrumbs />
+        </div>
         <BoxDetalhes carro={carro} />
       </div>
     </main>
