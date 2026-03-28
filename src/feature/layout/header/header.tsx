54aecf67f8390ceb40c3de486a1f010e414a380c
Gabriel Martins <gabrielmartins8802@gmail.com>
2026-03-28 12:42:38 -0300

    feat(ui): alertenar links de navegacao e icone de home baseado na rota

--- src/feature/layout/header/header.tsx
+++ src/feature/layout/header/header.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+  const pathname = usePathname();
+
+  const isHome = pathname === "/";
+
+  const HomeIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-white transition-colors"
+    >
+      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+      <polyline points="9 22 9 12 15 12 15 22"></polyline>
+    </svg>
+  );
+
   return (
     // Tornamos o header absoluto e transparente para ele "sentar" em cima do banner
     <header
@@ -13,7 +38,7 @@ export function Header() {
       */}
       <div className="flex bg-black/10 backdrop-blur-lg border-b border-white/10 justify-between items-center px-6 md:px-10 h-20">
         <Link href="/">
-          <div className="flex items-center gap-4 group">
+          <div className="flex items-center group">
             {/* Logo redimensionado para caber harmonicamente no header */}
             <div className="relative w-24 h-24 md:w-48 md:h-48 overflow-hidden">
               <Image
@@ -37,27 +62,37 @@ export function Header() {
           </div>
         </Link>
 
-        {/* Adicionei um Menu Simples para não parecer vazio no Desktop */}
-        <nav className="hidden md:flex gap-8 items-center text-white font-medium text-sm">
-          <Link
-            href="#veiculos"
-            className="hover:text-[#FF4100] transition-colors"
-          >
-            Veículos
-          </Link>
-          <Link
-            href="#sobre"
-            className="hover:text-[#FF4100] transition-colors"
-          >
-            Sobre
-          </Link>
-          <Link
-            href="#contato"
-            className="px-5 py-2 bg-[#FF4100] rounded-full hover:bg-[#e63a00] transition-all shadow-lg shadow-orange-900/20"
-          >
-            Contato
-          </Link>
-        </nav>
+        {/*  */}
+        {isHome && (
+          <nav className="hidden md:flex gap-8 items-center text-white font-medium text-sm">
+            <Link
+              href="#veiculos"
+              className="hover:text-[#FF4100] transition-colors"
+            >
+              Veículos
+            </Link>
+            <Link
+              href="#sobre"
+              className="hover:text-[#FF4100] transition-colors"
+            >
+              Sobre
+            </Link>
+            <Link
+              href="#contato"
+              className="px-5 py-2 bg-[#FF4100] rounded-full hover:bg-[#e63a00] transition-all shadow-lg shadow-orange-900/20"
+            >
+              Contato
+            </Link>
+          </nav>
+        )}
+
+        {!isHome && (
+          <nav className="flex items-center">
+            <Link href={"/"} title="voltar para a Home" className="bg-[#FF4100] rounded-full p-2 hover:bg-[#ff4000b4]">
+              <HomeIcon />
+            </Link>
+          </nav>
+        )}
       </div>
     </header>
   );
